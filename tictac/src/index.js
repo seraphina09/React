b31189b0944bb223ca675c3ea60734cfdd64ee05
Seraphina09 <103226787+seraphina09@users.noreply.github.com>
2022-05-25 15:48:47 +0900

    commit

--- tictac/src/index.js
+++ tictac/src/index.js
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
+
 function Square(props) {
   return (
     <button className="square" onClick={props.onClick}>
-      {props.value}
-    </button>
+    {props.value}
+  </button>
   );
 }
 
@@ -46,151 +49,200 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+
+
   constructor(props) {
     super(props);
     this.state = {
-      history: [
-        {
-          squares: Array(9).fill(null)
-        }
-      ],
+      history: [{
+        squares: Array(9).fill(null),
+      }],
+      xIsNext: true,
       stepNumber: 0,
-      xIsNext: true
+      
     };
-  }
+  }  
+
 
   async handleClick(i) {
     const squares = this.getCurrentBoard();
     if (this.calculateWinner(squares) || squares[i]) {
       return;
     }
-    squares[i] = this.state.xIsNext ? "X" : "O";
-
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    // 一部追加
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    // const current = history[history.length - 1];
-    
     let all_history = history.concat([{
       squares: squares
     }])
-    if(all_history.length === 3) {
+    if (all_history.length === 3) {
       all_history.shift();
     }
-
-    await this.setState({
+ 
+  await this.setState({
       history: all_history,
-      stepNumber : all_history.length -1,
+      stepNumber: all_history.length -1 ,
       xIsNext: !this.state.xIsNext,
     });
     this.cpuAction(squares);
   }
 
-
-  // 盤面取得
-  getCurrentBoard(){
-    const history = this.state.history.slice(0, this.state.stepNumber +1);
-    const current = history[history.length -1];
+  getCurrentBoard() {
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const current = history[history.length - 1];
     const squares = current.squares.slice();
-
     return squares;
-
   }
-
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
   jumpTo(step) {
+    const xIsNext = step !== this.state.stepNumber ? !this.state.xIsNext : this.state.xIsNext;
     this.setState({
       stepNumber: step,
-      xIsNext: (step % 2) === 0
+      xIsNext
     });
   }
 
-
   cpuAction(squares) {
-    if(this.calculateWinner(squares)) return;
-    let history = this.state.history.slice(0, this.state.stepNumber +1);
-
-    //空いているマスを取得
-    const posisbleHand = [];
+ 
+    if (this.calculateWinner(squares)) return;
+    let history = this.state.history.slice(0, this.state.stepNumber + 1);
+ 
+    // 空いているマスを取得
+    const possible_hands = [];
     let hand = squares.indexOf(null);
-    while (hand !== -1){
-      posisbleHand.push(hand);
-      hand = squares.indexOf(null, hand +1);
+    while (hand !== -1) {
+      possible_hands.push(hand);
+      hand = squares.indexOf(null, hand + 1);
     }
-
     // 空いているマスがなければ終了
-    if(posisbleHand.length === 0) return;
-    // 空いているマスのうちランダムで1マスを取得
-    const actionHand = posisbleHand[Math.floor(Math.random()*posisbleHand.length)];
-    // 選択した手で更新
-    squares[actionHand] = this.state.xIsNext ? 'X' : 'O';
-    history[history.length -1].squares = squares;
-
+    if (possible_hands.length === 0) return;
+    // 空いているマスのうちランダムで１マスを取得
+    const action_hand = possible_hands[Math.floor(Math.random()*possible_hands.length)];
+ 
+    // 選択した手で盤面を更新
+    squares[action_hand] = this.state.xIsNext ? 'X' : 'O';
+    history[history.length - 1].squares  = squares;
+ 
     this.setState({
       history,
       xIsNext: !this.state.xIsNext,
     });
-
   }
 
-  
-　calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+
 
 
   render() {
     const history = this.state.history;
-    const current = history[history.length -1];
+    const current = history[history.length - 1];
     const winner = this.calculateWinner(current.squares);
-
     let status;
     if (winner) {
-      status = "Winner: " + winner;
+      status = 'Winner: ' + winner;
     } else {
-      status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
-      const moves = history.length < 2 ? null :(
+    const moves = history.length < 2 ? null :(
       <button onClick={() => this.jumpTo(0)}>１回前へ戻る</button>
-    );
+  );
 
-    
-    return (
-      <div className="game">
+      return (
+        <div className="game">
         <div className="game-board">
           <Board
             squares={current.squares}
-            onClick={i => this.handleClick(i)}
+            onClick={(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
           <div>{status}</div>
-          {/* {<ol>todo</ol> } */}
           <ol>{moves}</ol>
+          <ol>{/* TODO */}</ol>
         </div>
       </div>
     );
   }
 }
 
+class Firstlast extends React.Component{
+  constructor (){
+    super();
+    this.state = {
+      name : "firstlast"
+      };
+
+      this.onValueChange = this.onValueChange.bind(this);
+      this.formSubmit = this.formSubmit.bind(this);
+    }
+
+    onValueChange(event){
+      this.setState({
+        selectedOption : event.target.value
+      });
+    }
+
+    formSubmit(event) {
+      event.preventDefault();
+      console.log(this.state.selectedOption)
+    }
+
+    render(){
+      return (
+        <form onSubmit={this.formSubmit}>
+          <div className ="radio" >
+            <label>
+              <input 
+                  type = "radio"
+                  value = "male"
+                  checked = {this.state.selectedOption === "male"}
+                  onChange = {this.onValueChange}
+                  />
+                  Male
+            </label>
+          </div>
+
+          <div className ="radio" >
+            <label>
+              <input 
+                  type = "radio"
+                  value = "female"
+                  checked = {this.state.selectedOption === "female"}
+                  onChange = {this.onValueChange}
+                  />
+                  Female
+            </label>
+          </div>
+
+          <button className='btn btn-default' type = "submit">
+            GameStart!
+          </button>
+        </form>
+      );
+    }
+  }
+
+
+
 // ========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(<Game />);
-
-
-
-
